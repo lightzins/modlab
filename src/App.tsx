456c10dfd298935e8ee804bc3d4819178f2aba2a
lightzins <279122250+lightzins@users.noreply.github.com>
2026-09-03 18:00:06 -0300

    Fix catalog photo lookup type

--- src/App.tsx
+++ src/App.tsx
@@ -615,7 +615,7 @@ function DesignLab({ user, onSignOut }: { user?: User | null; onSignOut?: () => 
     let active = true
     void Promise.all(visibleModels.map(async (item) => {
       const [make, ...model] = item.name.split(' ')
-      const result = await findModelImage(make, model.join(' ')).catch(() => ({}))
+      const result = await findModelImage(make, model.join(' ')).catch(() => ({ image: undefined }))
       return { name: item.name, image: result.image }
     })).then((found) => {
       if (!active) return
